dc1c7c3cb0ce96aa1f46f5cab717d4b6bbaafd16
Raymond <info@javaminor2015.com>
2017-01-09 12:34:53 +0100

    Project Setup Microservices

---
+++ CustomerService/src/main/java/com/infosupport/team2/CustomerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.infosupport.team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+}
