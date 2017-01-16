9945c57423f15ab5c212e24be59335797d539342
Raymond <info@javaminor2015.com>
2017-01-16 13:56:19 +0100

    customers now have id

--- OrderService/src/main/java/com/infosupport/team2/OrderServiceApplication.java
+++ OrderService/src/main/java/com/infosupport/team2/OrderServiceApplication.java
@@ -30,12 +30,14 @@ public class OrderServiceApplication {
 			orderRepository.deleteAll();
 
 			Customer customer1 = new Customer();
+			customer1.setId("1");
 			customer1.setName("Jan Meesters");
 			customer1.setPhone("0612345678");
 			customer1.setEmail("jan.meesters@gmail.com");
 			customer1.setAddress(new Address("Test straat 13", "Breda", "1234AB"));
 
 			Customer customer2 = new Customer();
+			customer2.setId("2");
 			customer2.setName("Piet Piraat");
 			customer2.setPhone("0687654321");
 			customer2.setEmail("piet.piraat@gmail.com");
