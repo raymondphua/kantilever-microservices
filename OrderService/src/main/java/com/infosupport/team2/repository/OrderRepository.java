ab45dab5dfc3f1712218b20f9af20d1126c2099b
Raymond <info@javaminor2015.com>
2017-01-16 11:13:08 +0100

    Order repository added

---
+++ OrderService/src/main/java/com/infosupport/team2/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.infosupport.team2.repository;
+
+import com.infosupport.team2.enums.Status;
+import com.infosupport.team2.model.Order;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Created by Raymond Phua on 16-1-2017.
+ */
+public interface OrderRepository extends MongoRepository<Order, String>, CustomOrderRepository {
+    public Order findById(String id);
+    public Order findByStatus(Status status);
+}
