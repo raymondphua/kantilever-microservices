ab75ef24d2954a77f342fc5d122799093927380a
tgixxer <robin_vj@hotmail.com>
2017-01-25 09:43:43 +0100

    Cleanup unused variable and TODO block based on Eriks comment from pullrequest.

--- OrderService/src/main/java/com/infosupport/team2/resource/OrderResource.java
+++ OrderService/src/main/java/com/infosupport/team2/resource/OrderResource.java
@@ -6,16 +6,12 @@ import com.infosupport.team2.model.Product;
 import com.infosupport.team2.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -25,11 +21,13 @@ import java.util.Map;
 @CrossOrigin(exposedHeaders = "Location")
 @RestController
 @RequestMapping(value = "/orders")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class OrderResource {
 
     @Autowired
     private OrderService orderService;
 
+    @PreAuthorize("hasAuthority('employee')")
     @RequestMapping(method = RequestMethod.GET)
     public List<Order> allOrdersWithStatus(@RequestParam Map<String,String> allRequestParams) {
         return orderService.getOrders(allRequestParams);
@@ -40,10 +38,12 @@ public class OrderResource {
         return orderService.findOne(id);
     }
 
+
     @RequestMapping(value = "/{id}/products", method = RequestMethod.GET)
     public List<Product> getProductsFromOrder(@PathVariable Long id) {
         return orderService.getProductsFromOrderId(id);
     }
+
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Order> createOrder(@RequestBody Order order) {
@@ -55,6 +55,7 @@ public class OrderResource {
                 .buildAndExpand(result.getId()).toUri();
         return ResponseEntity.created(location).build();
     }
+
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<Order> updateStatusFromOrder(@PathVariable Long id, @RequestBody ProcessOrderModel processOrderModel) {
--- OrderService/src/main/java/com/infosupport/team2/resource/StatusResource.java
+++ OrderService/src/main/java/com/infosupport/team2/resource/StatusResource.java
@@ -3,6 +3,7 @@ package com.infosupport.team2.resource;
 import com.infosupport.team2.enums.Status;
 import com.infosupport.team2.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ public class StatusResource {
     @Autowired
     private OrderRepository orderRepo;
 
+    @PreAuthorize("hasAuthority('employee')")
     @RequestMapping(method = RequestMethod.GET)
     public List<Status> allStatuses()
     {
