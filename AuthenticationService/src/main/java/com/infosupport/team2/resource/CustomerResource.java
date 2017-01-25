f2a343495be8d85d5f4efb4b65f72aeb57662674
martin <theebag77@gmail.com>
2017-01-25 10:08:16 +0100

    Password hashed in database.

--- AuthenticationService/src/main/java/com/infosupport/team2/resource/CustomerResource.java
+++ AuthenticationService/src/main/java/com/infosupport/team2/resource/CustomerResource.java
@@ -3,6 +3,7 @@ package com.infosupport.team2.resource;
 import com.infosupport.team2.model.User;
 import com.infosupport.team2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,8 +19,11 @@ public class CustomerResource {
     @Autowired
     UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @RequestMapping(value = "/oauth/registration", method = RequestMethod.POST)
     public void createCustomer(@RequestBody User user) {
-        userService.save(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
     }
 }
